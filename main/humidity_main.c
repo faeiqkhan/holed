0e8e091c1335edb1ff8c5f6f423b2620c8a0aabf
faeiqkhan <65024753+faeiqkhan@users.noreply.github.com>
2023-05-20 17:36:05 +0530

    Update humidity_main.c

--- main/humidity_main.c
+++ main/humidity_main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ssd1306/ssd1306.h"
+#include "oled/oled.h"
 #include "driver/i2c.h"
 
 #define I2C_MASTER_NUM I2C_NUM_0
 #define I2C_MASTER_FREQ_HZ 100000
 
-#define SSD1306_I2C_ADDR 0x3C
+#define OLED_I2C_ADDR 0x3C
 
-#define SSD1306_WIDTH 128
-#define SSD1306_HEIGHT 64
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
 
 void app_main()
 {
@@ -26,18 +26,18 @@ void app_main()
     i2c_param_config(I2C_MASTER_NUM, &i2c_config);
     i2c_driver_install(I2C_MASTER_NUM, I2C_MODE_MASTER, 0, 0);
 
-    ssd1306_t dev;
-    dev.width = SSD1306_WIDTH;
-    dev.height = SSD1306_HEIGHT;
-    dev.color = SSD1306_COLOR_WHITE;
+    oled_t dev;
+    dev.width = OLED_WIDTH;
+    dev.height = OLED_HEIGHT;
+    dev.color = OLED_COLOR_WHITE;
 
-    uint8_t *fb = (uint8_t *)malloc(SSD1306_WIDTH * SSD1306_HEIGHT / 8);
-    memset(fb, 0, SSD1306_WIDTH * SSD1306_HEIGHT / 8);
+    uint8_t *fb = (uint8_t *)malloc(OLED_WIDTH * OLED_HEIGHT / 8);
+    memset(fb, 0, OLED_WIDTH * OLED_HEIGHT / 8);
 
-    ssd1306_init(&dev);
+    oled_init(&dev);
 
-    ssd1306_draw_string(&dev, fb, &Font_7x10, 0, 0, "Humidity:", SSD1306_COLOR_WHITE, SSD1306_COLOR_BLACK);
-    ssd1306_draw_string(&dev, fb, &Font_7x10, 0, 16, "Temperature:", SSD1306_COLOR_WHITE, SSD1306_COLOR_BLACK);
+    oled_draw_string(&dev, fb, &Font_7x10, 0, 0, "Humidity:", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    oled_draw_string(&dev, fb, &Font_7x10, 0, 16, "Temperature:", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
     char humidity_str[16];
     char temperature_str[16];
@@ -47,10 +47,10 @@ void app_main()
     snprintf(humidity_str, sizeof(humidity_str), "%.1f %%", humidity);
     snprintf(temperature_str, sizeof(temperature_str), "%.1f C", temperature);
 
-    ssd1306_draw_string(&dev, fb, &Font_7x10, 64, 0, humidity_str, SSD1306_COLOR_WHITE, SSD1306_COLOR_BLACK);
-    ssd1306_draw_string(&dev, fb, &Font_7x10, 80, 16, temperature_str, SSD1306_COLOR_WHITE, SSD1306_COLOR_BLACK);
+    oled_draw_string(&dev, fb, &Font_7x10, 64, 0, humidity_str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    oled_draw_string(&dev, fb, &Font_7x10, 80, 16, temperature_str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 
-    ssd1306_display_frame_buffer(&dev, fb);
+    oled_display_frame_buffer(&dev, fb);
 
     free(fb);
 }
