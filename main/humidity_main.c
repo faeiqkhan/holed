bbde37c5087c841e02392eeb9587f9640d7c40fc
faeiqkhan <65024753+faeiqkhan@users.noreply.github.com>
2023-05-20 16:57:19 +0530

    Update humidity_main.c

--- main/humidity_main.c
+++ main/humidity_main.c
@@ -1,54 +1,53 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "esp_log.h"
-#include "dht.h"
-#include "ssd1306.h"
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_err.h>
+#include <dht/dht.h>
+#include <ssd1306/ssd1306.h>
 
-#define DHT_GPIO_PIN GPIO_NUM_4
-#define OLED_SDA_GPIO_PIN GPIO_NUM_5
-#define OLED_SCL_GPIO_PIN GPIO_NUM_6
+#define DHT_GPIO 5 // D1 pin
+#define OLED_SDA_GPIO 4 // D2 pin
+#define OLED_SCL_GPIO 15 // D8 pin
 
-void sensor_task(void *pvParameters) {
-    dht_t dht;
-    ssd1306_info_t oled_info;
+void temperature_task(void *arg)
+{
+    ESP_ERROR_CHECK(dht_init(DHT_GPIO, false));
 
-    // Initialize DHT11 sensor
-    dht_init(DHT_TYPE_DHT11, DHT_GPIO_PIN);
+    ssd1306_128x64_i2c_init(OLED_SDA_GPIO, OLED_SCL_GPIO);
+    ssd1306_display_on();
 
-    // Initialize OLED display
-    ssd1306_128x64_i2c_init(&oled_info, OLED_SDA_GPIO_PIN, OLED_SCL_GPIO_PIN);
-    ssd1306_clear_screen(&oled_info);
-    ssd1306_refresh(&oled_info, true);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-    while (1) {
-        // Read humidity and temperature from DHT11
-        float humidity, temperature;
-        int ret = dht_read_float_data(DHT_TYPE_DHT11, DHT_GPIO_PIN, &humidity, &temperature);
-        if (ret == ESP_OK) {
-            // Convert float values to strings
+    while (1)
+    {
+        int humidity = 0;
+        int temperature = 0;
+
+        if (dht_read_data(DHT_TYPE_DHT22, DHT_GPIO, &humidity, &temperature) == ESP_OK) {
             char humidity_str[10];
             char temperature_str[10];
-            snprintf(humidity_str, sizeof(humidity_str), "%.2f%%", humidity);
-            snprintf(temperature_str, sizeof(temperature_str), "%.2fC", temperature);
+            snprintf(humidity_str, sizeof(humidity_str), "%d%%", humidity);
+            snprintf(temperature_str, sizeof(temperature_str), "%dC", temperature);
 
-            // Clear OLED display
-            ssd1306_clear_screen(&oled_info);
+            ssd1306_clear();
 
-            // Print humidity and temperature on OLED
-            ssd1306_draw_string(&oled_info, 0, 0, "Humidity:");
-            ssd1306_draw_string(&oled_info, 80, 0, humidity_str);
-            ssd1306_draw_string(&oled_info, 0, 16, "Temperature:");
-            ssd1306_draw_string(&oled_info, 96, 16, temperature_str);
+            ssd1306_draw_string(0, 0, "Humidity:");
+            ssd1306_draw_string(80, 0, humidity_str);
+            ssd1306_draw_string(0, 16, "Temperature:");
+            ssd1306_draw_string(96, 16, temperature_str);
 
-            // Update OLED display
-            ssd1306_refresh(&oled_info, true);
+            ssd1306_refresh();
+        }
+        else {
+            printf("Fail to get DHT temperature data\n");
         }
 
-        vTaskDelay(pdMS_TO_TICKS(5000));  // Delay between readings
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
+
+    vTaskDelete(NULL);
 }
 
 void app_main() {
-    xTaskCreate(sensor_task, "sensor_task", 4096, NULL, 5, NULL);
+    xTaskCreate(temperature_task, "temperature task", 2048, NULL, tskIDLE_PRIORITY, NULL);
 }
