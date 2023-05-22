3747efca0c659322c26c763bd1675669fd645155
faeiqkhan <65024753+faeiqkhan@users.noreply.github.com>
2023-05-22 11:49:02 +0530

    Update humidity_main.c

--- main/humidity_main.c
+++ main/humidity_main.c
@@ -48,9 +48,9 @@ void temperature_task(void *arg)
             char display_text[16];
             snprintf(display_text, sizeof(display_text), "Humidity: %d%%", humidity);
             ssd1306_clear_screen(&dev);
-            ssd1306_draw_string(&dev, 0, 0, display_text, SSD1306_COLOR_WHITE);
+            ssd1306_draw_string(&dev, 0, 0, display_text, OLED_COLOR_WHITE);
             snprintf(display_text, sizeof(display_text), "Temperature: %dC", temperature);
-            ssd1306_draw_string(&dev, 0, 16, display_text, SSD1306_COLOR_WHITE);
+            ssd1306_draw_string(&dev, 0, 16, display_text, OLED_COLOR_WHITE);
             ssd1306_init(&dev);
         }
         else
